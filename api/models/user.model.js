adc2229ca189b1a3f3552d57a7568b60959f2943
bbamsarr <anasaxon123@gmail.com>
2023-10-22 14:10:47 +0200

    Create SignUp Api Route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,6 +17,6 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true});
 
-const User = mongooose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
