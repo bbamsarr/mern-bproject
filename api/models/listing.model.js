ee4cd30d2dee9f48a728c8a5257567e39a2ac6e8
bbamsarr <anasaxon123@gmail.com>
2024-01-04 13:54:19 +0100

    Create Pet List Page

---
+++ api/models/listing.model.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        species: {
+            type: String,
+            required: true,
+        },
+        breed: {
+            type: String,
+            required: true,
+        },
+        age: {
+            type: Number,
+            required: true,
+        },
+        size: {
+            type: String,
+            required: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+        },
+        vaccinated: {
+            type: Boolean,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        imageUrls: {
+            type: Array,
+            required: true,
+        },
+        userRef: {
+            type: String,
+            required: true,
+        },
+        //dodaj jos za vakcinaciju, starost zivotinje...
+    }, {timestamps: true}
+)
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
